8a4960e750e1b1d6f5d7147252f25c501a1b0f4b
wfnuser <wfnuser@126.com>
2018-08-02 21:31:04 +0800

    https://leetcode.com/problems/minimum-path-sum/description/

---
+++ 64.MinimumPathSum/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> sum(m, vector<int>(n, 0));
+        
+        sum[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            sum[i][0] = grid[i][0] + sum[i-1][0];
+        }
+        for (int j = 1; j < n; j++) {
+            sum[0][j] = grid[0][j] + sum[0][j-1];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                sum[i][j] = min(sum[i-1][j], sum[i][j-1]) + grid[i][j];
+            }
+        }
+        
+        return sum[m-1][n-1];
+    }
+};
