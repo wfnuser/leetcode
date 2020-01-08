33488b537ce7681eea0a2e3dfb21dc7952d633cf
wfnuser <wfnuser@126.com>
2020-01-08 23:24:38 +0800

    198

---
+++ 198.rob.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=cpp
+ *
+ * [198] 打家劫舍
+ *
+ * https://leetcode-cn.com/problems/house-robber/description/
+ *
+ * algorithms
+ * Easy (42.38%)
+ * Likes:    587
+ * Dislikes: 0
+ * Total Accepted:    65K
+ * Total Submissions: 153.4K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * 
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * 
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,2,3,1]
+ * 输出: 4
+ * 解释: 偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+ * 偷窃到的最高金额 = 1 + 3 = 4 。
+ * 
+ * 示例 2:
+ * 
+ * 输入: [2,7,9,3,1]
+ * 输出: 12
+ * 解释: 偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+ * 偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        if (size == 0) return 0;
+        if (size == 1) return nums[0];
+        if (size == 2) return max(nums[0], nums[1]);
+
+        int dp[nums.size()];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i-1], (dp[i-2] + nums[i]));
+        }
+
+        return dp[size-1];
+    }
+};
+// @lc code=end
+
