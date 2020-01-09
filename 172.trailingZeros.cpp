ae2b414ce885cb8d5d32dd630ec6a956d892b69d
wfnuser <wfnuser@126.com>
2020-01-09 13:01:09 +0800

    172

---
+++ 172.trailingZeros.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        while(n) {
+            ans += n / 5;
+            n /= 5;
+        }
+
+        return ans;
+    }
+};
