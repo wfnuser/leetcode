737e2337ecf3b5bee7c7c1fef03ef852efe7a947
wfnuser <wfnuser@126.com>
2020-01-21 13:30:36 +0800

    225

---
+++ 225.myStack.cpp
@@ -0,0 +1,45 @@
+class MyStack {
+public:
+    queue<int> q;
+
+    /** Initialize your data structure here. */
+    MyStack() {
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        q.push(x);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        queue<int> nq;
+        int size = q.size();
+        for (int i = 0; i < size - 1; i++) {
+            nq.push(q.front());
+            q.pop();
+        }
+        int res = q.front();
+        q = nq;
+        return res;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q.back();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
