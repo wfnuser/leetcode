d2f235df588691256f3f645122f2127770f35746
wfnuser <wfnuser@126.com>
2020-01-22 21:42:10 +0800

    374

---
+++ 374.guessNumber.cpp
@@ -0,0 +1,26 @@
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 1;
+        int r = n;
+
+        while(l < r) {
+            int mid = l + (r-l)/2;
+
+            if (guess(mid) == 0) return mid;
+
+            if (guess(mid) < 0) {
+                r = mid;
+            } else {
+                l = mid+1;
+            }
+        }
+
+        return l;
+    }
+};
