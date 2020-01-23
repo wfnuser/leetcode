cba06b73fed484326c5864e6a67bf4ced46c01a7
wfnuser <wfnuser@126.com>
2020-01-23 21:05:45 +0800

    190

---
+++ 190.reverseBits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            ans = ans << 1;
+            ans += (n&1);
+            n = n >> 1;
+        }
+
+        return ans;
+    }
+};
