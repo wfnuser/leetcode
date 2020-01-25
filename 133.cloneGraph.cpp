6134b033ac50d4311f843d5c4ad8d484a6177b21
wfnuser <wfnuser@126.com>
2020-01-25 23:43:28 +0800

    133

---
+++ 133.cloneGraph.cpp
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+};
+*/
+class Solution {
+public:
+    unordered_map<int, Node*> nodes;
+
+    Node* cloneGraph(Node* node) {
+        if (nodes[node->val]) return nodes[node->val];
+        Node* n = new Node();
+        n->val = node->val;
+        nodes[n->val] = n;
+
+        vector<Node*> cloneNeighbors;
+        for (auto neighbor: node->neighbors) {
+            cloneNeighbors.push_back(cloneGraph(neighbor));
+        }
+        n->neighbors = cloneNeighbors;
+ 
+        return n;
+    }
+};
