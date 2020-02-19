a84ff1775ce5190321d0ef5c078358a7e9897bd8
wfnuser <wfnuser@126.com>
2020-02-19 22:19:59 +0800

    468

---
+++ 468.validIpAddress.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String validIPAddress(String IP) {
+        if (IP == null) {
+            return "Neither";
+        }
+        
+        String regex0 = "(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])";
+        String regexIPv4 = regex0 + "(\\." + regex0 + "){3}";
+        String regex1 = "([\\da-fA-F]{1,4})";
+        String regexIPv6 = regex1 + "(:" + regex1 + "){7}";
+        
+        String result = "Neither";
+        if (IP.matches(regexIPv4)) {
+            result = "IPv4";
+        } else if (IP.matches(regexIPv6)) {
+            result = "IPv6";
+        }
+        return result;
+    }
+}
