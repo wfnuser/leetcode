88126e707c6c4b21de3e35652272199a9fcc7f02
wfnuser <wfnuser@126.com>
2020-04-10 23:38:54 +0800

    151

--- 151.reverseWords.cpp
+++ 151.reverseWords.cpp
@@ -1,24 +1,37 @@
 class Solution {
-public:
+   public:
     string reverseWords(string s) {
-        vector<string> words;
+        reverse(s.begin(), s.end());
 
+        string ss = "";
+        int i = 0;
+        while (i < s.size() && s[i] == ' ') {
+            i++;
+        }
+        ss = s.substr(i);
+        if (ss.size() == 0) return "";
+        while (ss[ss.size() - 1] == ' ') {
+            ss.pop_back();
+        }
+        s = ss;
+        ss = "";
         for (int i = 0; i < s.size(); i++) {
-            string tmp = "";
-            while(i < s.size() && s[i] != ' ') {
-                tmp = tmp + s[i];
-                i++;
+            ss.push_back(s[i]);
+            if (s[i] == ' ') {
+                while (i < s.size() && s[i] == ' ') i++;
+                i--;
             }
-            if (tmp != "") words.push_back(tmp);
         }
 
-        string ans = "";
-        
-        for (int i = words.size() - 1; i >= 0; i--) {
-            ans = ans + words[i];
-            if (i != 0) ans = ans + " ";
+        int l = 0;
+        for (int i = 0; i < ss.size(); i++) {
+            if (ss[i] == ' ') {
+                reverse(ss.begin() + l, ss.begin() + i);
+                l = i + 1;
+            }
         }
+        reverse(ss.begin() + l, ss.begin() + ss.size());
 
-        return ans;
+        return ss;
     }
 };
