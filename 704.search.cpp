d7a6635804d9f7090bd368bf80183c02a699b66f
wfnuser <wfnuser@126.com>
2020-04-12 12:39:35 +0800

    704

---
+++ 704.search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            if (left == right) {
+                if (nums[left] == target) return left;
+                return -1;
+            }
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
