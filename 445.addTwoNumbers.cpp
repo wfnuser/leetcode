54e7601a357b9526ffc78a62ceeeed42b29ad416
wfnuser <wfnuser@126.com>
2020-04-14 21:30:05 +0800

    445

--- 445.addTwoNumbers.cpp
+++ 445.addTwoNumbers.cpp
@@ -8,65 +8,70 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
-        if (head == NULL || head->next == NULL) return head;
-        ListNode* prev = head;
-        ListNode* curr = head->next;
-        prev->next = NULL;
-        while (curr != NULL) {
-            ListNode* node = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = node;
+
+    ListNode* reverse(ListNode* l) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* pre = dummy;
+        ListNode* cur = l;
+        dummy->next = l;
+
+        while(cur->next) {
+            ListNode* next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
         }
-        return prev;
+
+        cur->next = pre;
+        l->next = NULL;
+
+        return cur;
     }
 
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL) return l2;
+        if (l2 == NULL) return l1;
+
         ListNode* rl1 = reverse(l1);
         ListNode* rl2 = reverse(l2);
 
-        ListNode* p1 = rl1;
-        ListNode* p2 = rl2;
         ListNode* dummy = new ListNode(0);
         ListNode* p = dummy;
 
+        ListNode* p1 = rl1;
+        ListNode* p2 = rl2;
+
         int carry = 0;
-        while(p1&&p2) {
-            p->val = carry + p1->val + p2->val;
-            carry = p->val / 10;
-            p->val %= 10;
-            p->next = new ListNode(0);
+        while(p1 && p2) {
+            int sum = p1->val + p2->val + carry;
+            carry = sum / 10;
+            p->next = new ListNode(sum % 10);
             p = p->next;
             p1 = p1->next;
             p2 = p2->next;
         }
 
         while(p1) {
-            p->val = carry + p1->val;
-            carry = p->val / 10;
-            p->val %= 10;
-            p->next = new ListNode(0);
+            int sum = p1->val + carry;
+            carry = sum / 10;
+            p->next = new ListNode(sum % 10);
             p = p->next;
             p1 = p1->next;
         }
         while(p2) {
-            p->val = carry + p2->val;
-            carry = p->val / 10;
-            p->val %= 10;
-            p->next = new ListNode(0);
+            int sum = p2->val + carry;
+            carry = sum / 10;
+            p->next = new ListNode(sum % 10);
             p = p->next;
             p2 = p2->next;
         }
         if (carry) {
-            p->val = carry;
-            p->next = new ListNode(0);
+            p->next = new ListNode(carry);
             p = p->next;
         }
+        
+        ListNode* ans = reverse(dummy->next);
 
-        dummy = reverse(dummy);
-        return dummy->next;
+        return ans;
     }
-
-
 };
