438c5469c091826add5584ea42a614a86c336ca3
wfnuser <wfnuser@126.com>
2020-04-30 23:59:16 +0800

    203

---
+++ LCCI/2.03.deleteNode.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (node->next == NULL) {
+            node = NULL;
+        } else {
+            node->val = node->next->val;
+            node->next = node->next->next;
+        }
+    }
+};
