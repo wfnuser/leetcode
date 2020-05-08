9259847d9d6827e57108ceb46f63c4611157d78e
wfnuser <wfnuser@126.com>
2020-05-08 23:56:31 +0800

    221

--- 221.maximalSquare.cpp
+++ 221.maximalSquare.cpp
@@ -1,34 +1,3 @@
-/*
- * @lc app=leetcode.cn id=221 lang=cpp
- *
- * [221] 最大正方形
- *
- * https://leetcode-cn.com/problems/maximal-square/description/
- *
- * algorithms
- * Medium (38.72%)
- * Likes:    210
- * Dislikes: 0
- * Total Accepted:    19.2K
- * Total Submissions: 49.5K
- * Testcase Example:  '[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]'
- *
- * 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
- * 
- * 示例:
- * 
- * 输入: 
- * 
- * 1 0 1 0 0
- * 1 0 1 1 1
- * 1 1 1 1 1
- * 1 0 0 1 0
- * 
- * 输出: 4
- * 
- */
-
-// @lc code=start
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
@@ -50,17 +19,14 @@ public:
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == '1') { 
-                    dp[i][j] = 1;
-                    if (dp[i-1][j-1]>=1 && dp[i-1][j] >= dp[i-1][j-1] && dp[i][j-1] >= dp[i-1][j-1]) {
-                        dp[i][j] = dp[i-1][j-1] + 1;
-                    }
+                    dp[i][j] = min(min(dp[i-1][j-1], dp[i-1][j]), dp[i][j-1]) + 1;
                     ans = max(ans, dp[i][j]);
                 } else {
                     dp[i][j] = 0;
                 }
             }
         }
+
         return ans*ans;
     }
 };
-// @lc code=end
