f006412216e8f67e6d3dbda4b2d5c35a85a822fc
wfnuser <wfnuser@126.com>
2020-05-20 22:37:04 +0800

    1371

---
+++ 1371.findTheLongestSubstring.go
@@ -0,0 +1,34 @@
+func findTheLongestSubstring(s string) int {
+    
+    ans := 0;
+    status := 0;
+    pos := make([]int, 32);
+
+    for i := 0; i < len(pos); i++ {
+        pos[i] = -1;
+    }
+    pos[0] = 0;
+
+    for i := 0; i < len(s); i++ {
+        switch s[i] {
+            case 'a':
+                status ^= 1 << 0;
+            case 'e':
+                status ^= 1 << 1;
+            case 'i':
+                status ^= 1 << 2;
+            case 'o':
+                status ^= 1 << 3;
+            case 'u':
+                status ^= 1 << 4;
+        }
+
+        if pos[status] == -1 {
+            pos[status] = i + 1;
+        } else {
+            if (i + 1 - pos[status] - ans) > 0 { ans = i + 1 - pos[status] } 
+        }
+    }
+
+    return ans;
+}
