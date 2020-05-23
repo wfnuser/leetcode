bbae97436c6a7435ae88c4d23e291f5bd0fa8b38
wfnuser <wfnuser@126.com>
2020-05-23 16:18:32 +0800

    290

---
+++ 290.wordPattern.go
@@ -0,0 +1,30 @@
+import "strings"
+
+func wordPattern(pattern string, str string) bool {
+	words := strings.Fields(str);
+
+	wordMap := make(map[string]byte)
+	patternMap := make(map[byte]string)
+
+    if len(pattern) != len(words) {return false;}
+
+	for idx, word := range words {
+		ch := pattern[idx]
+		_, wordAppeared := wordMap[word]
+		_, patternAppeared := patternMap[ch]
+
+		if !wordAppeared && !patternAppeared {
+			wordMap[word] = ch;
+			patternMap[ch] = word;
+			continue;
+		}
+		
+		if !wordAppeared || !patternAppeared {
+			return false
+		}
+
+		if wordMap[word] != ch || patternMap[ch] != word { return false; }
+	}
+
+	return true;
+}
