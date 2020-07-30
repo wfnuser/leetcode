2e7b11dff09e08704b3223c6cf00f1e16ce8bc66
wfnuser <wfnuser@126.com>
2020-07-30 22:27:51 +0800

    28: rabin-karp

---
+++ 28.strStr.py
@@ -0,0 +1,30 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        d = 256
+        q = 9997
+        n = len(haystack)
+        m = len(needle)
+        h = pow(d,m-1)%q
+        p = 0
+        t = 0
+
+        if m > n:
+            return -1
+
+        for i in range(m): # preprocessing
+            p = (d*p+ord(needle[i]))%q
+            t = (d*t+ord(haystack[i]))%q
+        for s in range(n-m+1): # note the +1
+            if p == t: # check character by character
+                match = True
+                for i in range(m):
+                    if needle[i] != haystack[s+i]:
+                        match = False
+                        break
+                if match:
+                    return s
+            if s < n-m:
+                t = (t-h*ord(haystack[s]))%q
+                t = (t*d+ord(haystack[s+m]))%q
+                t = (t+q)%q
+        return -1
