4ea941ab3a7d161b2c38b4945a1b4d93337526f8
wfnuser <wfnuser@126.com>
2020-08-26 22:08:36 +0800

    860

---
+++ 860.lemonadeChange.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (auto bill: bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                five--;
+                ten++;
+            } else {
+                if (ten > 0) {
+                    ten--;
+                    five--;
+                } else {
+                    five -= 3;
+                }
+            }
+            if (five < 0) return false;
+        }
+
+        return true;
+    }
+};
