7b6f3442e10757cb31268fd51708b427402f73eb
wfnuser <wfnuser@126.com>
2020-12-30 16:58:08 +0800

    512

---
+++ 512.gamePlayAnalysis.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select player_id, device_id
+from Activity
+where (player_id, event_date) in (select player_id, min(event_date)
+                                  from Activity
+                                  group by player_id);
