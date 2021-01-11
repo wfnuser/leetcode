849fd4ebb6975784351ce5a3b7a8e7da2cded6a5
wfnuser <wfnuser@126.com>
2021-01-11 23:58:00 +0800

    1202

---
+++ 1202.smallestStringWithSwaps.cpp
@@ -0,0 +1,54 @@
+class DisjointSetUnion {
+private:
+    vector<int> f, rank;
+    int n;
+
+public:
+    DisjointSetUnion(int _n) {
+        n = _n;
+        rank.resize(n, 1);
+        f.resize(n);
+        for (int i = 0; i < n; i++) {
+            f[i] = i;
+        }
+    }
+
+    int find(int x) {
+        return f[x] == x ? x : f[x] = find(f[x]);
+    }
+
+    void unionSet(int x, int y) {
+        int fx = find(x), fy = find(y);
+        if (fx == fy) {
+            return;
+        }
+        if (rank[fx] < rank[fy]) {
+            swap(fx, fy);
+        }
+        rank[fx] += rank[fy];
+        f[fy] = fx;
+    }
+};
+
+class Solution {
+public:
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        DisjointSetUnion dsu(s.length());
+        for (auto& it : pairs) {
+            dsu.unionSet(it[0], it[1]);
+        }
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i < s.length(); i++) {
+            mp[dsu.find(i)].emplace_back(s[i]);
+        }
+        for (auto& [x, vec] : mp) {
+            sort(vec.begin(), vec.end(), greater<int>());
+        }
+        for (int i = 0; i < s.length(); i++) {
+            int x = dsu.find(i);
+            s[i] = mp[x].back();
+            mp[x].pop_back();
+        }
+        return s;
+    }
+};
