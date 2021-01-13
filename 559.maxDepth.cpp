e20ec03ab575761645a25fb6a6c7beb8994cc774
wfnuser <wfnuser@126.com>
2021-01-13 23:29:58 +0800

    559

---
+++ 559.maxDepth.cpp
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == NULL) return 0;
+        int depth = 0;
+
+        for (auto c: root->children) {
+            depth = max(depth, maxDepth(c));
+        } 
+
+        return 1+depth;
+    }
+};
