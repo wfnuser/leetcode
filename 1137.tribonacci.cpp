d5995e2bc44958f7b64a8d5d3b21ef415e54b2f1
wfnuser <wfnuser@126.com>
2021-08-08 20:25:43 +0800

    1137

---
+++ 1137.tribonacci.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        int a = 0, b = 1, c = 1;
+        for (int i = 3; i <= n; i++) {
+            int d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return c;
+    }
+};
