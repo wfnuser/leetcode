8fb3db8a52dcea057dd49e6c9c79b3baa13e170a
wfnuser <wfnuser@126.com>
2021-10-25 22:30:38 +0800

    373

--- 373.kSmallestPairs.cpp
+++ 373.kSmallestPairs.cpp
@@ -44,55 +44,55 @@
 //     }
 // };
 
-// solution2 which TLE
-class Solution {
+// solution2 
+class Solution {    
 public:
-    int count(vector<int>& nums1, vector<int>& nums2, int target) {
-        int cnt = 0;
-
-        for (int i = 0; i < nums1.size(); i++) {
-            int j = nums2.size() - 1;
-            while (j >= 0 && nums1[i] + nums2[j] > target) {
-                j--;
-            }
-            cnt += j + 1;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) 
+    {
+        long left = INT_MIN, right = INT_MAX;
+        while (left < right)
+        {
+            long mid = left+(right-left)/2;
+            long count = countSmallerOrEqual(mid, nums1, nums2);
+            if (count < k)
+                left = mid+1;
+            else
+                right = mid;
         }
+        int m = left;
 
-        return cnt;
+        vector<vector<int>>rets;
+        for (int i=0; i<nums1.size(); i++)
+        {
+            for (int j = 0; j<nums2.size() && nums1[i]+nums2[j]<m; j ++)
+            {
+                rets.push_back({nums1[i], nums2[j]});
+            }                
+        }
+        
+        for (int i=0; i<nums1.size(); i++)
+        {
+            for (int j = 0; j<nums2.size() && nums1[i]+nums2[j]<=m && rets.size() < k; j ++)
+            {
+                if (nums1[i]+nums2[j]==m) {
+                    rets.push_back({nums1[i], nums2[j]});
+                }
+            }                
+        }        
+        
+        return rets;
     }
 
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        long left = INT_MIN;
-        long right = INT_MAX;
-
-        
-        while (left < right) {
-            int cnt = count(nums1, nums2, (left + right) / 2);
-            if (cnt < k) {
-                left = (left + right) / 2 + 1;
-            } else {
-                right = (left + right) / 2;
-            }
+    long countSmallerOrEqual(int m, vector<int>& nums1, vector<int>& nums2)
+    {
+        int j = nums2.size()-1;
+        long ret = 0;
+        for (int i=0; i<nums1.size(); i++)
+        {
+            while (j>=0 && nums1[i]+nums2[j]>m)
+                j--;
+            ret += j+1;
         }
-
-        vector<vector<int>> ans;
-
-        for (int i = 0; i < nums1.size(); i++) {
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums1[i] + nums2[j] < left) {
-                    ans.push_back(vector<int>{nums1[i], nums2[j]});
-                }
-            }
-        }
-
-        for (int i = 0; i < nums1.size(); i++) {
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums1[i] + nums2[j] == left && ans.size() < k) {
-                    ans.push_back(vector<int>{nums1[i], nums2[j]});
-                }
-            }
-        }
-
-        return ans;
+        return ret;
     }
 };
